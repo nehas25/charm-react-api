7deb7c2bdadc36fb9ca1345f51526a0c87d0b1b3
neha-s25 <neha.s25@gmail.com>
2021-06-28 09:44:53 -0700

    fixed issue in account created in seeded data

--- controllers/setupdbController.js
+++ controllers/setupdbController.js
@@ -1,6 +1,6 @@
 const db = require('../models');
 const data = require('../dressDataImgPaths.json');
-
+const bcrypt = require('bcryptjs');
 
 function handleError(res, err, status = 400, message = 'Something went wrong. Please try again.') {
   console.log('Error in productController.js:', err);
@@ -28,6 +28,12 @@ async function resetdb(req, res) {
         username: "test",
         password: "test"
     }
+     // Hash the user's password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(userObj.password, salt);
+  
+    userObj.password = hashedPassword;
+    // Create a new user
     const userRes = await db.Account.create(userObj);
     console.log('Created user: ', userRes);
 
@@ -40,7 +46,7 @@ async function resetdb(req, res) {
 
     res.status(200).json(output);
 
-  } catch(error) {
+  } catch(err) {
     handleError(res, err);
   }
 }
